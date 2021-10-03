1ec4aee6c87a578b0a8a7e77a30347fbd53f4e1d
kara2703 <kabraanoushka02@gmail.com>
2021-10-03 11:37:58 -0400

    Update Player.ml

--- src/Player.ml
+++ src/Player.ml
@@ -4,10 +4,11 @@ type player_id = int
 
 type amount = int
 
+type location = string
+
 type player = {
   id : player_id;
-  current_amount : int;
+  current_amount : amount;
   properties : string list;
-
-
+  current_location : location;
 }

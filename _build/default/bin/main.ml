b7b1c9db67eeb7fdc1f3a2e841d16d216616b021
vianxnguyen <viviamguyen@gmail.com>
2021-10-08 17:15:26 -0400

    Add dune files and property type

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -1,1 +1,2 @@
+
 print_endline "testing dune"
---
+++ _build/default/src/property.ml
@@ -0,0 +1,14 @@
+
+type property_type = string
+type color = string
+type name = string 
+
+
+
+type property = { 
+  property_type : property_type; 
+  color : color;
+  name: name;
+  price: int;
+  rent: int
+}

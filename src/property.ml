6b347aec222f55b409d6bb4e40cb2cdea524ce2e
vianxnguyen <viviamguyen@gmail.com>
2021-10-12 16:59:02 -0400

    Update property and property mli with specs

--- src/property.ml
+++ src/property.ml
@@ -1,14 +1,21 @@
 
-type property_type = string
+type p_type = string
 type color = string
-type name = string 
-
 
 
 type property = { 
-  property_type : property_type; 
+  p_type : p_type; 
   color : color;
-  name: name;
+  name: string;
   price: int;
-  rent: int
+  rent: int;
+  owner : string (* change to optional player *)
 }
+
+let p_type p = p.p_type
+let color p = p.color
+let name p = p.name
+let price p = p.price
+let rent p = p.rent 
+let owner p = p.owner 
+let set_owner p o = {p with owner = o}
---
+++ src/property.mli
@@ -0,0 +1,32 @@
+(** Representation of property data.
+    This module represents the property data. *)
+
+type property
+(** The abstract type that represents a property *)
+
+type p_type = string
+(** The type of property. *)
+
+type color = string
+(** The type of property color. *)
+
+val p_type : property -> p_type 
+(** [p_type p] is the property type of property [p]. *)
+
+val color : property -> color 
+(** [color p] is the color of property [p]. *)
+
+val name : property -> string
+(** [name p] is the name of property [p]. *)
+
+val price : property -> int 
+(** [price p] is the price of property [p]. *)
+
+val rent : property -> int 
+(** [rent p] is the rent of property [p]. *)
+
+val owner : property -> string 
+(** [owner p] is the owner of property [p]. *)
+
+val set_owner : property -> string -> property
+(** [set_owner p o] is the property [p] with owner o. *)

383282b301f315b5d6d038b8cabcfcb44f77dce3
kara2703 <kabraanoushka02@gmail.com>
2021-10-22 14:06:55 -0400

    Create currency.ml

---
+++ src/currency.ml
@@ -0,0 +1,14 @@
+(* update players’ funds as they play - increase when others pay rent,
+ decrease when they land on others’ property, decrease when they 
+ purchase property etc.  *)
+open player.ml
+
+let pay_rent_helper rent_payer rent_taker rent = 
+  rent_payer.current_amount = rent_payer.current_amount - rent
+  rent_taker.current_amount = rent_taker.current_amount + rent
+let pay_rent property player =
+  let property_rent = property.rent in
+  let player_pay_rent = if property.owner = None then (**option to buy property*)
+  else pay_rent_helper player property.owner property_rent
+
+let 

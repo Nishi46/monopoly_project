bef230481affc331dd8606fc4258a4cc197e8e92
kara2703 <kabraanoushka02@gmail.com>
2021-10-30 19:44:56 -0400

    Update Player.ml

--- src/Player.ml
+++ src/Player.ml
@@ -16,13 +16,13 @@ type player = {
 
 let create_player_record_helper number = 
   {id= number; current_amount = 1500; properties = []; current_location = 1}
-let create_player_record no_of_players = 
+let create_player_record no_of_players acc = 
   match no_of_players with 
   | 0 -> raise InvalidPlayerNumber
-  | _ -> create_player_record_helper no_of_players
+  | _ -> (create_player_record_helper no_of_players) :: acc
 
-let player_records players_list = 
-  let 
+let player_records_list number acc = 
+  create_player_record number []
 let get_current_location player = player.current_location
 
 let get_player_id player = player.id

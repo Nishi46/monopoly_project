7e5369410ff0fe064ede7731f0794f01229ec03e
kara2703 <kabraanoushka02@gmail.com>
2021-10-30 20:11:38 -0400

    Update nothing.ml

--- src/nothing.ml
+++ src/nothing.ml
@@ -1,5 +1,15 @@
-open Property
-type nothing = property list
+open Yojson.Basic.Util
+
+type property_id = int
+type property_type = string
+type nothing = {
+  id : property_id;
+  p_type : property_type; 
+  color : string;
+  name: string;
+  price: int;
+  rent: int;
+}
 
 let nothing_of_json j =
   {
@@ -9,14 +19,15 @@ let nothing_of_json j =
     name= j |> member "name" |> to_string;
     price = j |> member "price" |> to_int;
     rent = j |> member "rent" |> to_int;
-    owner = None
   }
 
 type p = {
-    properties : property list 
+    properties : nothing list
   }
   
 let nothing_list_of_json j =
     {
-      properties = j |> member "properties" |> to_list |> List.map nothing_of_json;
+      properties = j |> member "property" |> to_list |> List.map nothing_of_json;
     }
+    
+let from_json json = nothing_of_json json
